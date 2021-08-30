2c73f4c0ced329d703556fd5cfac165e211f5793
bhunao <bhunao@gmail.com>
2021-08-29 21:48:12 -0300

    add: __repr__ method

--- pokemon.py
+++ pokemon.py
@@ -1,13 +1,32 @@
-from fastapi import FastAPI
-from data import get_pokemon_by_id
-
-api = FastAPI()
-
-@api.get("/")
-async def home():
-    return {"msg": "pokemon-api"}
+import pandas as pd
+from dataclasses import dataclass
 
 
-@api.get("/pokemon/{id}")
-async def get_pokemon(id: int):
-    return get_pokemon_by_id(id)
+@dataclass(frozen=True)
+class Pokemon:
+    """class that stores the pokemon data and has methods to compare and evaluate then"""
+    id: int
+    name: str
+    type: tuple
+    hp: int
+    attack: int
+    defense: int
+    sp_attack: int
+    sp_defense: int
+    speed: int
+    total: int
+    sprite: str = None
+    _is_mega: bool = None
+    _evolution: object = None
+    _devolution: object = None
+
+    def __repr__(self):
+        id = self.id
+        name = self.name
+        type = self.type
+
+        return f"<{id=} {name=} {type=}>"
+
+    def to_dict(self):
+        return dict()
+
